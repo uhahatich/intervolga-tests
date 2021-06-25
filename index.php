94e2614a127670ae5d1ff953cc1bb10a7272eee8
uhahatich <uhahatisch@gmail.com>
2021-06-25 19:15:17 +0300

    Start template

---
+++ index.php
@@ -0,0 +1,5 @@
+<? require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/header.php' ?>
+
+
+
+<? require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/footer.php' ?>
---
+++ vendor/footer.php
@@ -0,0 +1,6 @@
+<script src="/js/main.js"></script>
+<script src="/js/jquery.min.js"></script>
+
+</body>
+
+</html>
