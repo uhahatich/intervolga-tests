8003828e9f241b4756a5053f1e075ddf3018df3e
uhahatich <uhahatisch@gmail.com>
2021-06-29 14:18:49 +0300

    complet task 4

--- task4/index.php
+++ task4/index.php
@@ -1,5 +1,14 @@
 <? require $_SERVER['DOCUMENT_ROOT'] . '/vendor/header.php' ?>
-
-
+<? include $_SERVER['DOCUMENT_ROOT'] . '/php-scripts/task4.php' ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <? print_r($arr) ?>
+        </div>
+        <div class="col-md-6">
+            <p class="">Количество последовательных пар одинаковых элементов: <?= $counter ?></p>
+        </div>
+    </div>
+</div>
 
 <? require $_SERVER['DOCUMENT_ROOT'] . '/vendor/footer.php' ?>
